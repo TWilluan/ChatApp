3e935599be42b07290a1e48479dff47e0f06efb5
Tuan Vo <tuanvo.will@gmail.com>
2024-02-17 22:54:27 -0500

    create message and covnersation model

---
+++ backend/models/convers.model.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+    {
+        participant: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+
+        messages: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Message",
+                default: [],
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const conversation = mongoose.model("Conversation", conversationSchema);
+
+export default conversation;
