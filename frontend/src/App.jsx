6f36c31c9c4a2464369afae83507606b199d3f74
Tuan Vo <tuanvo.will@gmail.com>
2024-02-20 02:25:27 -0500

    implement signup and frontend'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <button class="btn">Button</button>
-      <button class="btn btn-neutral">Neutral</button>
-      <button class="btn btn-primary">Primary</button>
-      <button class="btn btn-secondary">Secondary</button>
-      <button class="btn btn-accent">Accent</button>
-      <button class="btn btn-ghost">Ghost</button>
-      <button class="btn btn-link">Link</button>
-    </>
-  )
+	return (
+		<>
+			<div className='p-4 h-screen flex items-center justify-center'>
+				<SignUp/>
+			</div>
+		</>
+	)
 }
 
 export default App
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,38 @@
+
+
+
+const Login = () => {
+    return (<>
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+            <div className="h-full w-full bg-blue-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 border border-gray-100">
+                <h1 className="text-3x1 font-semibold text-center text-gray-300">Login 
+                    <span className="text-blue-500"> ChatApp</span>
+                </h1>
+
+                <form>
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text">UserName</span>
+                        </label>
+                        <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10"></input>
+                    </div>
+
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10"></input>
+                    </div>
+
+                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                        Don't have an account?
+                    </a>
+
+                    <button className="btn btn-block btn-sm mt-2">Login</button>
+                </form>
+            </div>
+        </div>
+    </>);
+}
+
+export default Login;
