e02f1b51279644a86f00fd05f9df0f249de51e27
Tuan Vo <tuanvo.will@gmail.com>
2024-02-23 05:00:14 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from "path"
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -11,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 import connect from "./db/connectToMongoDb.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,6 +24,11 @@ app.use(cookieParser()); //parse cookie
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // start listening
 server.listen(PORT, () => {
--- frontend/src/hooks/useLogIn.js
+++ frontend/src/hooks/useLogIn.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
-import { json } from "react-router-dom";
 
 const useLogIn = () => {
     const [loading, setLoading] = useState(false);
     const { setAuthUser } = useAuthContext();
 
     const login = async (username, password) => {
-        const sucess = handleInputErrors({ username, password });
-        if (!sucess) return;
-        
-        setLoading(true);
+        const success = handleInputErrors({username, password});
+        if (!success) return;
 
+        setLoading(true);
         try {
-            const res = await fetch("api/auth/login", {
+            const res = await fetch("/api/auth/login", {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
                 body: JSON.stringify({
@@ -24,12 +22,14 @@ const useLogIn = () => {
             });
 
             const data = await res.json();
-            if (data.error) throw new Error(data.error);
+            if (data.error) {
+                throw new Error(data.error);
+            }
 
             localStorage.setItem("authUser", JSON.stringify(data));
             setAuthUser(data);
         } catch (error) {
-            console.log("Error in use login");
+            console.log("Error in useLogin");
             toast.error(error);
         } finally {
             setLoading(false);
